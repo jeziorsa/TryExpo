72922da60a8ffede1be68b6b664e29cf053727a4
Jeziorsa <biuro@atsa.pl>
2013-05-07 19:46:31 +0200

    niom

--- tryexpo.py
+++ tryexpo.py
@@ -3,7 +3,6 @@ import datetime
 import urllib
 import webapp2
 
-from google.appengine.ext import db
 from google.appengine.api import users
 
 from google.appengine.ext import blobstore
@@ -18,75 +17,6 @@ import os
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
-MAIN_PAGE_FOOTER_TEMPLATE = """\
-    <form action="/sign?%s" method="post">
-      <div><textarea name="content" rows="3" cols="60"></textarea></div>
-      <div><input type="submit" value="Sign Guestbook"></div>
-    </form>
-    <hr>
-    <form>Guestbook name: <input value="%s" name="guestbook_name">
-    <input type="submit" value="switch"></form>
-  </body>
-</html>
-"""
-
-class Greeting(db.Model):
-    """Models an individual Guestbook entry with author, content, and date."""
-    author = db.StringProperty()
-    content = db.StringProperty(multiline=True)
-    date = db.DateTimeProperty(auto_now_add=True)
-
-
-def guestbook_key(guestbook_name=None):
-    """Constructs a Datastore key for a Guestbook entity with guestbook_name."""
-    return db.Key.from_path('Guestbook', guestbook_name or 'default_guestbook')
-
-
-class MainPage(webapp2.RequestHandler):
-
-    def get(self):
-        guestbook_name=self.request.get('guestbook_name')
-        greetings_query = Greeting.all().ancestor(
-            guestbook_key(guestbook_name)).order('-date')
-        greetings = greetings_query.fetch(10)
-
-        if users.get_current_user():
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
-
-        template_values = {
-            'greetings': greetings,
-            'url': url,
-            'url_linktext': url_linktext,
-        }
-
-        template = JINJA_ENVIRONMENT.get_template('index.html')
-        self.response.write(template.render(template_values))
-
-
-class Guestbook(webapp2.RequestHandler):
-
-    def post(self):
-        # We set the same parent key on the 'Greeting' to ensure each greeting
-        # is in the same entity group. Queries across the single entity group
-        # will be consistent. However, the write rate to a single entity group
-        # should be limited to ~1/second.
-        guestbook_name = self.request.get('guestbook_name')
-        greeting = Greeting(parent=guestbook_key(guestbook_name))
-
-        if users.get_current_user():
-            greeting.author = users.get_current_user().nickname()
-
-        greeting.content = self.request.get('content')
-        greeting.put()
-
-        query_params = {'guestbook_name': guestbook_name}
-        self.redirect('/?' + urllib.urlencode(query_params))
-
-
 class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
     def post(self):
         upload_files = self.get_uploads('file')
@@ -99,16 +29,26 @@ class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
         if not blobstore.get(blob_key):
             self.error(404)
         else:
-            self.send_blob(blobstore.BlobInfo.get(blob_key), save_as=True)
+            self.send_blob(blobstore.BlobInfo.get(blob_key))
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
-        upload_url = blobstore.create_upload_url('/upload')
-        self.response.out.write('<html><body>')
-        self.response.out.write('<form action="%s" method="POST" enctype="multipart/form-data">' % upload_url)
-        self.response.out.write("""Upload File: <input type="file" name="file"><br> <input type="submit" name="submit" value="Submit"> </form></body></html>""")
+        # upload_url = blobstore.create_upload_url('/upload')
+        # self.response.out.write('<html><body>')
+        # self.response.out.write('<form action="%s" method="POST" enctype="multipart/form-data">' % upload_url)
+        # self.response.out.write("""Upload File: <input type="file" name="file"><br> <input type="submit" name="submit" value="Submit"> </form></body></html>""")
+        images_list = blobstore.BlobInfo.all()
 
-        for b in blobstore.BlobInfo.all():
-            self.response.out.write('<li><a href="/serve/%s' % str(b.key()) + '">' + str(b.filename) + '</a>')
 
-app = webapp2.WSGIApplication([('/', MainPage), ('/file', MainHandler), ('/sign', Guestbook), ('/upload', UploadHandler), ('/serve/([^/]+)?', ServeHandler)], debug=True)
+        template_values = {
+            'images': images_list,
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+
+
+        # for b in blobstore.BlobInfo.all():
+        #     self.response.out.write('<li><img src="/serve/%s' % str(b.key()) + '">' + str(b.filename) + '</a>')
+
+app = webapp2.WSGIApplication([('/', MainHandler), ('/upload', UploadHandler), ('/serve/([^/]+)?', ServeHandler)], debug=True)
